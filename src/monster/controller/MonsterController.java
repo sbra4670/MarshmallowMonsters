15fe478099b661ff605dddf3f93f5636e277f162
Sebastian <31518719+sbra4670@users.noreply.github.com>
2017-09-19 12:40:54 -0600

    used else, if, and if-else statments

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -9,10 +9,10 @@ public class MonsterController
 	{
 		MarshmallowMonster basic = new MarshmallowMonster();
 		System.out.println(basic);
-		MarshmallowMonster fred = new MarshmallowMonster("Silly Fred Monster", 7, 3, 6.7, true);
+		MarshmallowMonster fred = new MarshmallowMonster("Silly Fred Monster", 7, 8, 6.7, true);
 		System.out.println(fred);
 		System.out.println("I am feeling hungry, I am going to eat one of fred's arms");
-		fred.setArmCount(fred.getArmCount() - 4);
+		fred.setArmCount(fred.getArmCount() - 1);
 		System.out.println(fred);
 
 		interactWithMonster(fred);
@@ -20,10 +20,29 @@ public class MonsterController
 	private void interactWithMonster(MarshmallowMonster currentMonster)
 	{
 		Scanner myScanner = new Scanner(System.in);
-		System.out.println(currentMonster()+" wants to know how many eyes you want to eat, please type in how many");
+		System.out.println(currentMonster.getName()+" wants to know how many eyes you want to eat, please type in how many");
 		int consumed = myScanner.nextInt();
 		currentMonster.setEyeCount(currentMonster.getEyeCount() - consumed);
 		System.out.println(currentMonster);
+		
+		System.out.println("How many arms are you interested in eating?, I have " + currentMonster.getArmCount());
+		//consumed = myScanner.nextInt();
+		int armEat = myScanner.nextInt();
+		
+		if(armEat == 0) {
+			System.out.println("Not hungry? oh so sad...");
+		}
+		else if (armEat<0) {
+			System.out.println("Reality is hard for you - it is impossible to eat a negative amount");
+		}
+		else if (armEat - currentMonster.getArmCount() < 0) {
+			System.out.println("You are not allowed to eat more than exists on me :/");
+		}
+		else
+		{
+			currentMonster.setArmCount(currentMonster.getArmCount()- armEat);
+			System.out.println("OK, now I have this many arms" + currentMonster.getArmCount());
+		}
 		myScanner.close();
 	}
 }
